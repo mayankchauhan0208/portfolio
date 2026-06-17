df1bd19e8feac7265edd328a1509bb2e7a17a796
mayankchauhan0208 <85047614+mayankchauhan0208@users.noreply.github.com>
2026-06-17 14:51:01 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
@@ -13,12 +11,7 @@ const nextConfig = {
       }
     ]
   },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isGithubPages ? "/portfolio" : ""
-  },
   trailingSlash: true,
-  basePath: isGithubPages ? "/portfolio" : "",
-  assetPrefix: isGithubPages ? "/portfolio/" : undefined,
   experimental: {
     optimizePackageImports: ["lucide-react", "framer-motion"]
   }
